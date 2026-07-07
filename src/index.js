1e42e662cc0c900e42a4e9eeac2b19170710fcd3
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2026-07-07 00:43:25 -0400

    Add styles to modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const taskDiv = document.querySelector(".tasks");
+const taskAddButton = document.querySelector(".add-button");
+const taskDialog = document.querySelector(".task-dialog");
+const closeButton = document.querySelector("close-modal");
+
+function addTaskScreen(task) {
+  const taskButton = document.createElement("button");
+  taskButton.textContent = task.title;
+}
+
+function showTaskModal() {
+  taskDialog.showModal();
+}
+
+function closeModal() {
+  taskDialog.close();
+}
+
+taskAddButton.addEventListener("click", showTaskModal);
+closeButton.addEventListener("click", closeModal);
